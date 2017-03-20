4e290e420f2742fdb7cc6e2e5f69d2e2ef223618
fattyduck <dison.ruan@yahoo.com>
2017-03-20 11:20:48 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,15 @@
+class Spaceship{
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.docked = crew.length <= 0;
+    this.phasersCharge = 'uncharged';
+    this.warpDrive = 'disengaged';
+    crew.forEach((member) => member.currentShip = this)
+  }
+
+}
